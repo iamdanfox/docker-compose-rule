f60a348c7cd1bba52178ff54bf92eea8f348da6d
Dan Fox <dfox@palantir.com>
2016-05-13 16:44:03 +0100

    Automatically insert correct license in Eclipse

--- src/test/java/com/palantir/docker/compose/example/ExampleSeleniumTest.java
+++ src/test/java/com/palantir/docker/compose/example/ExampleSeleniumTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.palantir.docker.compose.example;
 
 import static com.palantir.docker.compose.example.MyServices.SELENIUM;
